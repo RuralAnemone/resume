486c491c22fe99f04313a574a437ed53f6f074b0
Isaiah <64103005+RuralAnemone@users.noreply.github.com>
2024-06-24 16:39:25 -0500

    re-clean

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
-const root = document.querySelector(':root');
+const root = document.querySelector(":root");
 
 setInterval(updateRandomCSSVariables, 10);
 
 function updateRandomCSSVariables() {
     // console.log(Math.random());
-    root.style.setProperty('--random-time', `${Math.random()}s`);
-    root.style.setProperty('--random-rotation', `${Math.random()}deg`);
-    root.style.setProperty('--random-length', `${Math.random()}em`);
+    root.style.setProperty("--random-time", `${Math.random()}s`);
+    root.style.setProperty("--random-rotation", `${Math.random()}deg`);
+    root.style.setProperty("--random-length", `${Math.random()}em`);
 }
 
-document.querySelectorAll('.switch').forEach(e => {
-    e.addEventListener('click', () => {
-        if (e.classList.contains('on')) {
-            e.classList.add('off');
-            e.classList.remove('on');
+document.querySelectorAll(".switch").forEach(e => {
+    e.addEventListener("click", () => {
+        if (e.classList.contains("on")) {
+            e.classList.add("off");
+            e.classList.remove("on");
         } else {
-            e.classList.add('on');
-            e.classList.remove('off');
+            e.classList.add("on");
+            e.classList.remove("off");
         }
 
         const switchType = e.parentElement.classList[0];
         document.body.classList.contains(switchType) ? document.body.classList.remove(switchType) : document.body.classList.add(switchType);
 
-        if (switchType === 'nerd') nerdToggle(document.body.classList.contains('nerd'));
+        if (switchType === "nerd") nerdToggle(document.body.classList.contains("nerd"));
     });
 });
 
 // nerd mode
 
 function nerdToggle(nerdMode) {
-    const directionBinarywards = nerdMode ? 'to' : 'from';
+    const directionBinarywards = nerdMode ? "to" : "from";
 
     // iterate through all elements (small amount so who cares how, right?)
     // for every element that has text directly inside, tun that text through textToBinary()
-    document.querySelectorAll('*').forEach(e => {
+    document.querySelectorAll("*").forEach(e => {
         if (e.childNodes[0]?.nodeType === Node.TEXT_NODE) {
             const textNode = e.childNodes[0];
             textNode.nodeValue = binaryConvert(textNode.nodeValue, directionBinarywards);
@@ -42,15 +42,15 @@ function nerdToggle(nerdMode) {
 }
 
 function binaryConvert(text, directionBinarywards) {
-    if (directionBinarywards === 'to') {
+    if (directionBinarywards === "to") {
         return text
-            .split('')
+            .split("")
             .map(c => c.charCodeAt(0).toString(2))
-            .join(' ');
-    } else if (directionBinarywards === 'from') {
+            .join(" ");
+    } else if (directionBinarywards === "from") {
         return text
-            .split(' ')
+            .split(" ")
             .map(bin => String.fromCharCode(parseInt(bin, 2)))
-            .join('');
-    } else throw new Error('🐗🏿🤯🏼');
+            .join("");
+    } else throw new Error("🐗🏿🤯🏼");
 }
