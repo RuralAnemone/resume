e6254d94d687f9d4a430e60dd8f47dca8c8cde57
Isaiah <64103005+RuralAnemone@users.noreply.github.com>
2024-06-24 14:09:50 -0500

    no hidden

--- script.js
+++ script.js
@@ -8,7 +8,3 @@ function updateRandomCSSVariables() {
     root.style.setProperty('--random-rotation', `${Math.random()}deg`);
     root.style.setProperty('--random-length', `${Math.random()}em`);
 }
-
-document.querySelectorAll(".hidden").forEach(e => {
-    e.addEventListener('hover', e.remove);
-})
